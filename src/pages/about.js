f5a1af7adf8b46da786ec12278b83cd0529c7e66
fj <castelloiv@gmail.com>
2020-04-05 20:11:44 -0400

    styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import Header from "../components/header"
 
 export default () => (
     <div style={{ color: `teal `}}>
-        <Header />
+        <Header headerText="About Grind" />
         <p>I made this site while under quarantine for COVID-19. 
             I had made it through five interviews/tests for an awesome JavaScript Developer and had my final paired coding test coming up on hackerrank. Needles to say, everyone canceled everything. I decided to do some studying for the CompTIA Security+ exam, and was not happy with a lot of the resources. This blog is just documenting what I learn. Cheers!
         </p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default () => (
     <div>
         <h1>Welcome to the Grind</h1>
+        <Link to="/contact">Contact</Link>
         <p>Repitition is key</p>
     </div>
 )
